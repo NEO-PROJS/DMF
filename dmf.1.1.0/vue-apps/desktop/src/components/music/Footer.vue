52659bc36b457a770d94b1ddeccca70e439d4148
NEO-PROJS <neo.docs.21@gmail.com>
2023-04-23 16:01:24 +0330

    c-16

--- dmf.1.1.0/vue-apps/desktop/src/components/music/Footer.vue
+++ dmf.1.1.0/vue-apps/desktop/src/components/music/Footer.vue
@@ -1,6 +1,17 @@
 <template>
 <footer id="footer">
 
+    <div id="social-media">
+        <ul id="social-media-ul">
+            <li v-for="item, key in socialMedia" :key="key">
+                {{ item.name }}
+            </li>
+        </ul>
+    </div>
+    
+    <div id="more">
+    </div>
+
 </footer>
 </template>
 
@@ -8,6 +19,16 @@
 
 export default {
     name: 'FooterComp',
+    data(){
+        return {
+            socialMedia: [
+                {name:"telegram", id:"", img:""},
+                {name:"instagram", id:"", img:""},
+                {name:"whatsapp", id:"", img:""},
+                {name:"whatsapp", id:"", img:""}
+            ]
+        }
+    }
 }
 
 </script>
--- dmf.1.1.0/vue-apps/desktop/src/components/music/docks/MainPlayerRight.vue
+++ dmf.1.1.0/vue-apps/desktop/src/components/music/docks/MainPlayerRight.vue
@@ -1,33 +1,5 @@
 <template>
 <div id="main-player-right">
-    <div id="options">
-        <div id="cover">
-            <img :src="require('@/assets/img/music/dido.jpg')" />
-        </div>
-        <button
-            :class="{'opt-btn':true, 'left':true, 'active-shuffle':options.shuffle, 'deactive-shuffle':!options.shuffle}"
-            @click="options.shuffle = !options.shuffle">
-        </button>
-        <button
-            :class="{'opt-btn':true, 'right':true, 'active-like':options.like, 'deactive-like':!options.like}"
-            @click="options.like = !options.like">
-        </button>
-        <button
-            :class="{'opt-btn':true, 'left':true, 'active-add':options.add, 'deactive-add':!options.add}"
-            @click="options.add = !options.add">
-        </button>
-        <button class="opt-btn right" id=""
-            :class="{'opt-btn':true, 'left':true, 'active-album':options.albums, 'deactive-albums':!options.albums}">
-        </button>
-    </div>
-    <div id="timer">
-        <div id="curr"></div>
-    </div>
-    <div id="ctrl">
-        <button class="prev-next-btn" id="prev"></button>
-        <button id="play" disabled ></button>
-        <button class="prev-next-btn" id="next"></button>
-    </div>
 </div>
 </template>
 
@@ -51,31 +23,4 @@ export default {
 
 <style scoped>
 @import '@/assets/css/music/dock/mainPlayerRight.css';
-#main-player-right #options .deactive-shuffle {
-    background-image: url('@/assets/img/music/dockRight/shuffleDeAct.png');
-    opacity: 0.4;
-}
-#main-player-right #options .active-shuffle {
-    background-image: url('@/assets/img/music/dockRight/shuffleAct.png');
-}
-#main-player-right #options .deactive-like {
-    background-image: url('@/assets/img/music/dockRight/likeDeAct.png');
-    background-size: 80%;
-    opacity: 0.4;
-}
-#main-player-right #options .active-like {
-    background-size: 80%;
-    background-image: url('@/assets/img/music/dockRight/likeAct.png');
-}
-#main-player-right #options .deactive-add {
-    background-image: url('@/assets/img/music/dockRight/addDeAct.png');
-    opacity: 0.4;
-    transition: all 0.1s ease-in-out;
-}
-#main-player-right #options .active-add {
-    background-image: url('@/assets/img/music/dockRight/addDeAct.png');
-    transition: all 0.1s ease-in-out;
-    opacity: 1;
-    transform: rotate(-90deg);
-}
 </style>
