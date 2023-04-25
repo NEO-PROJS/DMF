a200996006595f9ec19061ee30c142f5d0151d71
NEO-PROJS <neo.docs.21@gmail.com>
2023-04-25 18:11:03 +0330

    c-18

--- dmf.1.1.0/vue-apps/desktop/src/components/music/TopIntro.vue
+++ dmf.1.1.0/vue-apps/desktop/src/components/music/TopIntro.vue
@@ -1,10 +1,20 @@
 <template>
 <div id="top-intro">
+    <div id="sign">
+        <ul>
+            <li><router-link :to="{name:'accountLogin'}" class="sign-router">
+                <p>ورود</p>
+            </router-link></li>
+            <li><router-link :to="{name:'accountSignup'}" class="sign-router">
+                <p>ثبت نام</p>
+            </router-link></li>
+        </ul>
+    </div>
     <div id="title">
-        <RouterLink :to="{name:'home'}" class="title-link">
+        <router-link :to="{name:'home'}" class="title-link">
         <p id="deep">Deep</p>
         <p id="music">Music</p>
-        </RouterLink>
+        </router-link>
     </div>
 </div>
 </template>
